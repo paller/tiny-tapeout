11ba17a2c0a6bb42fbda10087b5e91b34d2e3bab
Palle Ravn <pravn@chip.localdomain>
2022-09-01 21:38:55 +0200

    Test for SystemVerilog
    I guess it should work if Yosys is up for it.

--- src/user_module_341625365772370516.sv
+++ src/user_module_341625365772370516.sv
@@ -1,17 +1,14 @@
-`default_nettype none
-
 module user_module_341625365772370516(
-  input wire [7:0] io_in,
-  output wire [7:0] io_out
+  input  logic [7:0] io_in,
+  output logic [7:0] io_out
 );
 
-  wire clk;
   assign clk = io_in[0];
 
-  reg [4:0] result;
+  logic [4:0] result;
   assign io_out[4:0] = result;
 
-  always @(posedge clk) begin
+  always_ff @(posedge clk) begin
     result <= io_in[7:4] + io_in[3:0];
   end
 
